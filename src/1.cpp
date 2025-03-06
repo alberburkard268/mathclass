24bc6bd0346b6956369cf0c91e9879ad5e258e86
alberburkard268 <alberburkard268@gmail.com>
2025-03-06 12:19:29 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    if (num % 2 == 0) {
+        cout << "The number is even.";
+    } else {
+        cout << "The number is odd.";
+    }
+    
+    return 0;
+}
