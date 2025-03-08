42e8096948fc4cc8bac1bd80d9060342feecb6f5
alberburkard268 <alberburkard268@gmail.com>
2025-03-08 21:35:15 +0000

    Add 3.cpp

---
+++ src/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int num1 = 4;
+    int num2 = 5;
+    cout << "The sum of " << num1 << " and " << num2 << " is " << num1 + num2 << endl;
+    return 0;
+}
