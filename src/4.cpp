f454acacdd063819c9b0ed2797da1bdc51eceff4
alberburkard268 <alberburkard268@gmail.com>
2025-03-09 08:35:40 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 0;
+    cout << "Enter an integer number: ";
+    cin >> num;
+    if (num % 2 == 0) {
+        cout << "Even" << endl;
+    } else {
+        cout << "Odd" << endl;
+    }
+    return 0;
+}
