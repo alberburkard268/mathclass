5b1042a2f9365d0c5ddbe3ae6a673776e5cd4bce
alberburkard268 <alberburkard268@gmail.com>
2025-03-22 15:44:26 +0000

    Add 17.cpp

---
+++ src/17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 10.5;
+    float z = 2.7f;
+    string s = "Hello, World!";
+    bool b = true;
+
+    cout << "Value of x: " << x << endl;
+    cout << "Value of y: " << y << endl;
+    cout << "Value of z: " << z << endl;
+    cout << "Value of s: " << s << endl;
+    cout << "Boolean value b: " << b << endl;
+
+    return 0;
+}
