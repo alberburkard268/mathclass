ead714043afa67331be08277a946581b4a55e02e
alberburkard268 <alberburkard268@gmail.com>
2025-04-07 04:22:15 +0000

    Add 27.cpp

---
+++ src/27.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 2.5;
+    float z = 10.0f;
+
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+    cout << "z = " << z << endl;
+
+    return 0;
+}
