794920c1c5dcbaf5aa1e785c0fa864a9d24a0a73
alberburkard268 <alberburkard268@gmail.com>
2025-05-27 06:25:08 +0000

    Add 44.cpp

---
+++ src/44.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+    int a = 5;
+    double b = 3.14;
+    float c = 2.7f;
+    
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
+}
